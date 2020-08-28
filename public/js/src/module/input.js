cc48e15a03e5fbdeacedced0e9dcba02242d536f
Martijn van de Rijdt <martijn@enketo.org>
2020-08-28 17:03:09 -0600

    fixed: obtaining constraint expressions

--- public/js/src/module/input.js
+++ public/js/src/module/input.js
@@ -6,7 +6,10 @@ import inputModule from 'enketo-core/src/js/input';
 inputModule.getConstraint = function( control ) {
     // The array index corresponds to the number used in oc:constraint
     // Note if a spot is missing, e.g. constraint11, this will (deliberately) result in an undefined value,
-    return Array.from( Array( 21 ) ).map( ( val, i ) => i === 0 ? control.dataset.constraint : control.dataset.oc[`constraint${i}`] );
+
+    return Array.from( Array( 21 ) )
+        .map( ( val, i ) => i === 0 ? control.dataset.constraint : control.dataset[`ocConstraint${i}`] )
+        .filter( val => !!val );
 };
 
 
