afd40f701bab0819a78e481c12068234c643aac3
Krikor Krumlian <kkrumlian@users.noreply.github.com>
2023-10-13 13:38:26 -0400

    Revert "Fix/pdf analog scale 671"

--- app/lib/headless-browser.js
+++ app/lib/headless-browser.js
@@ -12,7 +12,7 @@ class BrowserHandler {
         const launchBrowser = async () => {
             this.browser = false;
             this.browser = await puppeteer.launch({
-                headless: 'new',
+                headless: true,
                 devtools: false,
                 args,
                 userDataDir,
